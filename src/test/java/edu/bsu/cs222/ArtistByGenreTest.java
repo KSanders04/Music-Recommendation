fb31b3940eedcae8afc031fa34136972c62cf129
KSanders04 <kasanders2004@gmail.com>
2024-10-24 23:20:48 -0400

    Refactor code for readability. Improve test readability. Delete readme file.

--- src/test/java/edu/bsu/cs222/ArtistByGenreTest.java
+++ src/test/java/edu/bsu/cs222/ArtistByGenreTest.java
@@ -2,23 +2,16 @@ package edu.bsu.cs222;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class tests {
+public class ArtistByGenreTest {
 
         @Test
         public void testGetArtistByGenre() {
             ArtistByGenre artistByGenre = new ArtistByGenre();
             try {
-                artistByGenre.getArtistByGenre("pop");  // Replace "pop" with any genre you want to test
+                artistByGenre.getArtistByGenre("pop");
             } catch (Exception e) {
                 fail("Exception thrown: " + e.getMessage());
             }
