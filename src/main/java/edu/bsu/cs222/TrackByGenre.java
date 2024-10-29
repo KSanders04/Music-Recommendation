64a56ed77a0cf2f2b13290742cb179ea1ef9d6e6
KSanders04 <kasanders2004@gmail.com>
2024-10-29 09:59:29 -0400

    Add a class to get tracks. Add a class to parse tracks and print neatly. Refactor code.

--- src/main/java/edu/bsu/cs222/TrackByGenre.java
+++ src/main/java/edu/bsu/cs222/TrackByGenre.java
@@ -5,12 +5,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class ArtistByGenre {
+public class TrackByGenre {
 
     RequestToken requestToken = new RequestToken();
-    ArtistParser artistParser = new ArtistParser();
+    TrackParser trackParser = new TrackParser();
 
-    public void getArtistByGenre(String genre) throws Exception {
+    public void getTrackByGenre(String genre) throws Exception {
         String accessToken = requestToken.getAccessToken();
         String url = "https://api.spotify.com/v1/recommendations?limit=5&seed_genres=" + genre + "&min_popularity=50";
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
@@ -25,7 +25,7 @@ public class ArtistByGenre {
                 while ((line = bufferedReader.readLine()) != null) {
                     response.append(line);
                 }
-                artistParser.printArtists(response.toString());
+                trackParser.printTrack(response.toString());
             }
         } else {
             System.out.println("GET request failed: " + connection.getResponseMessage());
--- src/main/java/edu/bsu/cs222/TrackParser.java
+++ src/main/java/edu/bsu/cs222/TrackParser.java
@@ -3,16 +3,16 @@ package edu.bsu.cs222;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ArtistParser {
-    public void printArtists(String jsonResponse) {
+public class TrackParser {
+    public void printTrack(String jsonResponse) {
         JSONObject jsonObject = new JSONObject(jsonResponse);
         JSONArray tracks = jsonObject.getJSONArray("tracks");
 
         if (!tracks.isEmpty()) {
             int numberOfArtists = 0;
-            System.out.println("Artists:");
+            System.out.println("Tracks:");
             for (int i = 0; i < tracks.length() && numberOfArtists < 5; i++) {
-                JSONArray artists = tracks.getJSONObject(i).getJSONArray("artists");
+                JSONArray artists = tracks.getJSONObject(i).getJSONArray("name");
                 for (int j = 0; j < artists.length() && numberOfArtists < 5; j++) {
                     String artistName = artists.getJSONObject(j).getString("name");
                     System.out.println(artistName);
@@ -20,7 +20,8 @@ public class ArtistParser {
                 }
             }
         } else {
-            System.out.println("No artists found for this genre.");
+            System.out.println("No tracks found for this genre.");
         }
     }
+}
 }
