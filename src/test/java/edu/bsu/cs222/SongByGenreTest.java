8719ef863e1f4ab6ed6ef3c799d3dbdd579edf60
KSanders04 <kasanders2004@gmail.com>
2024-10-30 12:44:54 -0400

    Add test to get the song by genre to assert true.

--- src/test/java/edu/bsu/cs222/SongByGenreTest.java
+++ src/test/java/edu/bsu/cs222/SongByGenreTest.java
@@ -1,12 +1,31 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class SongByGenreTest {
 
     @Test
-    public void testSongByGenre() {
-
+    public void testGetSongsByGenre() {
         SongByGenre songByGenre = new SongByGenre();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        try {
+            songByGenre.getSongByGenre("pop");
+
+            String output = outContent.toString();
+            Assertions.assertTrue(output.contains("Songs"));
+
+        } catch (Exception e) {
+            fail("Exception thrown: " + e.getMessage());
+        } finally {
+            System.setOut(System.out);
+        }
     }
 }
