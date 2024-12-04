07ffa8a3b9e31ab9709a50b9f9b9f5aa2480e03f
antho <anthoneelemar@gmail.com>
2024-12-04 03:10:21 -0500

    Add new artistbygenre

--- src/main/java/edu/bsu/cs222/ArtistByGenre.java
+++ src/main/java/edu/bsu/cs222/ArtistByGenre.java
@@ -4,21 +4,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.io.IOException;
+import org.json.JSONObject;
 
-@SuppressWarnings("ALL")
 public class ArtistByGenre {
 
-    RequestToken requestToken = new RequestToken();
-    ArtistParser artistParser = new ArtistParser();
+    RequestToken requestToken = new RequestToken(); // Get the API key from CredentialsLoader
+    ArtistParser artistParser = new ArtistParser(); // Assuming ArtistParser parses the artist data from JSON response
 
-    public String getArtistByGenre(String genre) throws Exception {
-        String accessToken = requestToken.getAccessToken();
-        String url = "https://api.spotify.com/v1/recommendations?limit=5&seed_genres=" + genre + "&min_popularity=45";
+    // Method to get artists by genre
+    public String getArtistByGenre(String genre) throws IOException {
+        String accessToken = requestToken.getAccessToken();  // Get the access token (API key)
+
+        String url = "https://api.jamendo.com/v3.0/tracks/?client_id=" + accessToken + "&format=json&limit=all&fuzzytags=lounge+classical+electronic+jazz+pop+hiphop+relaxation+rock+songwriter+world+metal+soundtrack&groupby=artist_id";
+
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
         connection.setRequestMethod("GET");
-        connection.setRequestProperty("Authorization", "Bearer " + accessToken);
 
         int responseCode = connection.getResponseCode();
+
         if (responseCode == HttpURLConnection.HTTP_OK) {
             try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 StringBuilder response = new StringBuilder();
@@ -26,9 +30,22 @@ public class ArtistByGenre {
                 while ((line = bufferedReader.readLine()) != null) {
                     response.append(line);
                 }
-                return artistParser.printArtists(response.toString());
+
+                System.out.println("Full Response: " + response.toString());
+
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                if (jsonResponse.has("results") && jsonResponse.getJSONArray("results").length() > 0) {
+                    return artistParser.printArtists(response.toString());
+                } else {
+                    System.out.println("No tracks found in the response.");
+                    return "No tracks found in the response.";
+                }
             }
+        } else {
+            System.out.println("Error: " + responseCode);
+            return "Failed to fetch tracks with response code: " + responseCode;
         }
-        return accessToken;
+
+
     }
 }
